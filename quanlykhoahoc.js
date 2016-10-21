affa5ad203dfca02febd8b39279348c47eee6ed0
BachTuan <bachtuan1991@gmail.com>
2016-10-21 18:42:21 +0700

    upp

---
+++ quanlykhoahoc.js
@@ -0,0 +1,22 @@
+$('#khoahoc').css("background","#e8f5e9");
+function khoahocshow() {
+	$('#content-quanly').html('<div class="my-course"><h2>Khóa Học HTML/CSS/JS</h2><p>Tác giả: Đặng QUang Huy</p></div> <div class="my-course"><h2>Khóa Học IOS</h2><p>Tác giả: Trịnh Minh Cường</p></div> <div class="my-course"><h2>Khóa Học PHP</h2><p>Tác giả: Thân Văn Nghĩa</p></div> <div class="my-course"><h2>Khóa Học NODEJS</h2><p>Tác giả: Phan Đức Việt</p></div>');
+	$('#khoahoc').css("background","#e8f5e9");
+	$('#taikhoan').css("background","#00e676");
+	$('#giaodich').css("background","#00e676");
+	$('#xephang').css("background","#00e676");
+}
+function taikhoanshow() {
+	$('#content-quanly').html('<table> <tr> <td>Họ Tên</td> <td>Bạch Thanh Tuấn</td> <td>Sửa</td> </tr> <tr> <td>Email</td> <td>bachtuan1991@gmail.com</td> <td>Sửa</td> </tr> <tr> <td>Mật Khẩu</td> <td>************</td> <td>Sửa</td> </tr> <tr> <td>Số Điện Thoại</td> <td>096-220-1946</td> <td>Sửa</td> </tr> <tr> <td>Số Dư Tài Khoản</td> <td>Bạch Thanh Tuấn</td> <td>Nạp Tiền</td> </tr> </table>');
+	$('#khoahoc').css("background","#00e676");
+	$('#taikhoan').css("background","#e8f5e9");
+	$('#giaodich').css("background","#00e676");
+	$('#xephang').css("background","#00e676");
+}
+function giaodichshow() {
+	$('#content-quanly').html('<table> <tr> <td>Khóa Học Đã Mua</td> <td>Thời Gian Mua</td> <td>Thời Gian Thực Tập</td> </tr> </table> <table> <tr> <td>HTML/CSS/JS</td> <td>15:24 24/09/2016</td> <td>0 Ngày</td> </tr> </table>');
+	$('#khoahoc').css("background","#00e676");
+	$('#taikhoan').css("background","#00e676");
+	$('#giaodich').css("background","#e8f5e9");
+	$('#xephang').css("background","#00e676");
+}
